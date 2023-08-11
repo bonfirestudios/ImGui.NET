07a719af5ab6b52bdac6915b5cb751a55af46fc5
Sam Christiansen <sam@bonfirestudios.com>
2023-08-11 14:14:53 -0700

    adding unity package

---
+++ Unity/com.imgui.net/Runtime/Unsafe.cs
@@ -0,0 +1,35 @@
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace ImGuiNET
+{
+    public static class Unsafe
+    {
+        public static unsafe ref T AsRef<T>(void* ptr) where T : struct => ref UnsafeUtility.AsRef<T>(ptr);
+        
+        public static int SizeOf<T>() where T : struct => UnsafeUtility.SizeOf<T>();
+
+        public static unsafe void CopyBlock(ref byte destination, ref byte source, uint byteCount)
+        {
+            UnsafeUtility.MemCpy(UnsafeUtility.AddressOf(ref destination), UnsafeUtility.AddressOf(ref source), byteCount);
+        }
+
+        public static unsafe void CopyBlock(void* destination, void* source, uint byteCount)
+        {
+            UnsafeUtility.MemCpy(destination, source, byteCount);
+        }
+
+        public static unsafe void InitBlockUnaligned(void* startAddress, byte value, uint byteCount)
+        {
+            UnsafeUtility.MemSet(startAddress, value, byteCount);
+        }
+
+        public static unsafe T Read<T>(void* source) where T : struct
+        {
+            T value = default;
+            
+            UnsafeUtility.MemCpy(source, UnsafeUtility.AddressOf(ref value), UnsafeUtility.SizeOf<T>());
+
+            return value;
+        }
+    }
+}
